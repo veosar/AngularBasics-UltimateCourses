13fe3480eeeb2571e2186a84ba2ad4ed22cd71c4
Shadaia <przemek.kijak5@gmail.com>
2023-12-17 15:39:14 +0100

    nested routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,10 @@ import { DonutListComponent } from './admin/containers/donut-list/donut-list.com
 import { DonutSingleComponent } from './admin/containers/donut-single/donut-single.component';
 
 export const routes: Routes = [
-  { path: '', component: DonutListComponent },
-  { path: 'donut', component: DonutSingleComponent}
+  { path: 'admin', children: [
+    { path: 'donuts', component: DonutListComponent},
+    { path: 'donut', component: DonutSingleComponent}
+  ]},  
 ];
 
 @NgModule({
